31b02d9aebcce976a4636c0faebfa7930521ba5d
Alina Malyutina <linamalutina1@gmail.com>
2022-10-29 22:46:05 +0300

    add squaring numbers entered by user

---
+++ square/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear(); // CLEAR TERMINAL
+Console.WriteLine("This programm read your number and squares it.");
+Console.WriteLine("Enter your number. It must be an integer");
+int number = Convert.ToInt32(Console.ReadLine()); // read number
+
+ int square = number * number; //calculation
+
+Console.WriteLine($"Square of {number} is {square}"); // output
+
+Console.WriteLine("Thanks for using my program <3"); //Thanks
