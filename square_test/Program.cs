fa3ac42396632bccb06b3933f2061ad84b76f18f
Alina Malyutina <linamalutina1@gmail.com>
2022-10-29 23:13:03 +0300

    add comparing square

---
+++ square_test/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Thie programm compares two numbers and check is first number square of second or not. ");
+
+Console.Write("Enter first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int square = num2 * num2;
+
+if(num1 == square)
+{
+    Console.WriteLine($"{num1} is square for {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} is not square for {num2}");
+}
+Console.WriteLine("Thanks for using my program <3");
