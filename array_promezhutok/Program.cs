24d7d8d81fde1fa4aae0bc814901eaa8ae299e6f
Alina Malyutina <linamalutina1@gmail.com>
2022-10-29 23:57:54 +0300

    idk how i can named it. print between -5 and 5 or another number how user entered

---
+++ array_promezhutok/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("This programm show all integer numbers between [-entered number] and [entered number]");
+Console.WriteLine("Enter your number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int size = number * 2 + 1;
+
+number = number * (-1);
+
+for(int i = 0; i < size; i++)
+{
+    Console.Write($"{number}, ");
+    number++;
+}
