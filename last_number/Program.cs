eeb596f3351f3ee9ac8db2e0dc418b1c89e28862
Alina Malyutina <linamalutina1@gmail.com>
2022-10-30 00:04:46 +0300

    Print last digit of number

---
+++ last_number/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("This program print last digit of number");
+Console.WriteLine("Enter your number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit = number % 10;
+
+Console.WriteLine($"Last digit of number {number} is {digit}");
+
+Console.WriteLine("Thanks for using my program <3");
